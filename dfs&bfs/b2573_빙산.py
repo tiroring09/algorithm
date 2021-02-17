3a4bfb1f54e77470f740cd34e5f0d7b3e230602e
tiroring <tiroring09@gmail.com>
2021-02-17 12:59:05 +0900

    백준2573빙산

--- dfs&bfs/b2573_빙산.py
+++ dfs&bfs/b2573_빙산.py
@@ -2,6 +2,8 @@ import sys
 sys.stdin = open('b2573.txt')
 
 from collections import deque
+# import sys
+# sys.setrecursionlimit(100000)
 
 N, M = map(int, input().split())
 matrix = [ list(map(int, input().split())) for _ in range(N) ]
@@ -32,20 +34,35 @@ def countWater(x, y):
     return cnt
 
 def isOne():
+    # 현시점 빙산이 한 덩어리인지 여부 출력. 이 때 빙산이 몇 덩어리인지 완전탐색한 후, 결과를 출력하는 방식을 택하면 python3에서 시간초과 및 recursion오류. pypy3에서는 통과.
+    # 두번째 빙산덩어리를 발견하는 순간 탈출하는 방식을 택해도 python3에서 시간초과 확인. 아예 다른방식으로 접근해야 할 듯.
     visit = [ [False] * M for _ in range(N) ]
-    cnt = 0
+    foundOne = False
     for i in range(N):
         for j in range(M):
             if matrix[i][j] != 0 and not visit[i][j]:
-                cnt += 1
-                visit[i][j] = True
-                # dfs(i, j, visit)
-                bfs(i, j, visit)
-    return True if cnt == 1 else False
+                if foundOne:
+                    return False
+                else:
+                    foundOne = True
+                    visit[i][j] = True
+                    # dfs(i, j, visit)
+                    bfs(i, j, visit)
+    return True
+
+# def isOne():
+#     visit = [ [False] * M for _ in range(N) ]
+#     cnt = 0
+#     for i in range(N):
+#         for j in range(M):
+#             if matrix[i][j] != 0 and not visit[i][j]:
+#                 cnt += 1
+#                 visit[i][j] = True
+#                 # dfs(i, j, visit)
+#                 bfs(i, j, visit)
+#     return True if cnt == 1 else False
 
 def dfs(x, y, visit):
-    # visit[x][y] = True
-
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
@@ -65,7 +82,6 @@ def bfs(x, y, visit):
                 visit[nx][ny] = True
                 dq.append((nx, ny))
 
-
 def isMeltAll():
     for i in range(N):
         for j in range(M):
@@ -78,9 +94,9 @@ def main():
     while True:
         year += 1
         melt()
-        if not isOne():
-            return year
         if isMeltAll():
             return 0
+        if not isOne():
+            return year
 
 print(main())
